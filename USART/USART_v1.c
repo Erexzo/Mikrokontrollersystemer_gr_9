d53488a90d66f2c69b3438d414495fc6857044af
anjo93 <90632944+anjo93@users.noreply.github.com>
2023-03-12 13:22:44 +0100

    Create USART_v1.c

---
+++ USART/USART_v1.c
@@ -0,0 +1,31 @@
+#include "USART_v1.h"
+
+static void USART3_sendChar(char c) //Oppretter en bruker-definert strøm
+{
+	while (!(USART3.STATUS & USART_DREIF_bm)) //sjekker at forrige sending er feridg
+	{
+		;
+	}
+	USART3.TXDATAL = c; //sender/skriver en karakter til USART registerert
+}
+
+//pakker inn _sendChar funksjonen slik at den møter forventningene til FDEV_SETUP_STREAM funksjonen
+static int USART3_printChar(char c, FILE *stream)
+{
+	USART3_sendChar(c);
+	return 0;
+}
+
+//tar en brukererdefinert buffer strøm og setter den opp som en strøm som er gyldig for stdio operasjoner
+static FILE USART_stream = FDEV_SETUP_STREAM(USART3_printChar, NULL, _FDEV_SETUP_WRITE);
+
+static void USART3_init(void)
+{
+	PORTB.DIR |= PIN0_bm; //aktiverer TX pin
+	
+	USART3.BAUD = (uint16_t)USART3_BAUD_RATE(9600); //setter baudrate
+	
+	USART3.CTRLB |= USART_TXEN_bm; //skrur på TX
+	
+	stdout = &USART_stream; //endrer standard output stream til brukerdefinert stream
+}
